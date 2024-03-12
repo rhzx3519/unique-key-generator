e7ae2bc5599745f0c44d4b97e95fd76b90dcd7d7
Zhenghao Lou <rhzx3519@gmail.com>
2024-03-12 23:05:31 +1100

    feat: new sequencer with option

--- pool/pool.go
+++ pool/pool.go
@@ -1,93 +1,93 @@
 package pool
 
 import (
-    "context"
-    "fmt"
-    "rhzx3519/unique-key-generator/sequencer"
+	"context"
+	"fmt"
+	"rhzx3519/unique-key-generator/sequencer"
 )
 
 const SEQ_SPAN = 100
 
 type IsExistParam struct {
-    key     string
-    existed chan bool
+	key     string
+	existed chan bool
 }
 
 type Pool struct {
-    cache         []string
-    currentSeq    int64
-    sequencer     sequencer.Sequencer
-    keyStream     chan string
-    isExistStream chan IsExistParam
+	cache         []string
+	currentSeq    int64
+	sequencer     sequencer.Sequencer
+	keyStream     chan string
+	isExistStream chan IsExistParam
 }
 
 func NewPool() *Pool {
-    seq, _ := sequencer.NewMongoSequencer()
-    return &Pool{
-        keyStream:     make(chan string),
-        isExistStream: make(chan IsExistParam),
-        sequencer:     seq,
-    }
+	seq, _ := sequencer.NewSequencer(sequencer.Option{IsMemory: false})
+	return &Pool{
+		keyStream:     make(chan string),
+		isExistStream: make(chan IsExistParam),
+		sequencer:     seq,
+	}
 }
 
 func (p *Pool) close() {
-    close(p.keyStream)
-    close(p.isExistStream)
-    p.sequencer.Reset()
+	close(p.keyStream)
+	close(p.isExistStream)
+	p.sequencer.Reset()
 }
 
 func (p *Pool) Run(ctx context.Context) {
-    go func() {
-        defer fmt.Println("Pool closure exited.")
-        defer p.close()
-        for {
-            select {
-            case p.keyStream <- p.generate():
-            case param := <-p.isExistStream:
-                param.existed <- p.checkExist(param.key)
-            case <-ctx.Done():
-                return
-            }
-        }
-    }()
+	go func() {
+		defer fmt.Println("Pool closure exited.")
+		defer p.close()
+		for {
+			select {
+			case p.keyStream <- p.generate():
+			case param := <-p.isExistStream:
+				param.existed <- p.checkExist(param.key)
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
 }
 
 func (p *Pool) Key() string {
-    return <-p.keyStream
+	return <-p.keyStream
 }
 
 func (p *Pool) IsExist(key string) bool {
-    param := IsExistParam{
-        key:     key,
-        existed: make(chan bool),
-    }
-    defer close(param.existed)
-    p.isExistStream <- param
-    return <-param.existed
+	param := IsExistParam{
+		key:     key,
+		existed: make(chan bool),
+	}
+	defer close(param.existed)
+	p.isExistStream <- param
+	return <-param.existed
 }
 
 func (p *Pool) generate() string {
-    if len(p.cache) != 0 {
-        r := p.cache[0]
-        p.cache = p.cache[1:]
-        return r
-    }
-    // generate new keys and add them into cache
-    seq, _ := p.sequencer.Current()
-    for i := 0; i < SEQ_SPAN; i++ {
-        p.cache = append(p.cache, base64Encode(seq+int64(i)))
-    }
-    err := p.sequencer.Save(seq + SEQ_SPAN)
-    if err != nil {
-        panic(err)
-    }
-    p.currentSeq = seq + SEQ_SPAN
+	if len(p.cache) != 0 {
+		r := p.cache[0]
+		p.cache = p.cache[1:]
+		return r
+	}
+	// generate new keys and add them into cache
+	seq, _ := p.sequencer.Current()
+	for i := 0; i < SEQ_SPAN; i++ {
+		p.cache = append(p.cache, base64Encode(seq+int64(i)))
+	}
+	err := p.sequencer.Save(seq + SEQ_SPAN)
+	if err != nil {
+		panic(err)
+	}
+	p.currentSeq = seq + SEQ_SPAN
 
-    r := p.cache[0]
-    p.cache = p.cache[1:]
-    return r
+	r := p.cache[0]
+	p.cache = p.cache[1:]
+	return r
 }
 
 func (p *Pool) checkExist(key string) bool {
-    return base64Decode(key) < p.currentSeq
+	return base64Decode(key) < p.currentSeq
 }
--- sequencer/sequencer.go
+++ sequencer/sequencer.go
@@ -1,97 +1,108 @@
 package sequencer
 
 import (
-    "context"
-    "errors"
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-    "os"
+	"context"
+	"errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
 )
 
+type Option struct {
+	IsMemory bool
+}
+
 type Sequencer interface {
-    Current() (int64, error)
-    Save(seq int64) error
-    Reset() error
+	Current() (int64, error)
+	Save(seq int64) error
+	Reset() error
 }
 
 type MongoSequence struct {
-    Name  string `bson:"name,omitempty"`
-    Value int64  `bson:"value,omitempty"`
+	Name  string `bson:"name,omitempty"`
+	Value int64  `bson:"value,omitempty"`
 }
 
 const (
-    DB_NAME       = "sample_sequence"
-    COLL_NAME     = "sequences"
-    SEQUENCE_NAME = "universal_sequence"
+	DB_NAME       = "sample_sequence"
+	COLL_NAME     = "sequences"
+	SEQUENCE_NAME = "universal_sequence"
 )
 
 type MongoSequencer struct {
-    client *mongo.Client
+	client *mongo.Client
+}
+
+func NewSequencer(option Option) (Sequencer, error) {
+	if option.IsMemory {
+		return NewMemorySequencer()
+	}
+	return NewMongoSequencer()
 }
 
 func NewMongoSequencer() (*MongoSequencer, error) {
-    uri := os.Getenv("MONGODB_URI")
-    var err error
-    client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
-    if err != nil {
-        return nil, err
-    }
-    return &MongoSequencer{
-        client: client,
-    }, nil
+	uri := os.Getenv("MONGODB_URI")
+	var err error
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+	return &MongoSequencer{
+		client: client,
+	}, nil
 }
 
 func (s *MongoSequencer) Current() (int64, error) {
-    coll := s.client.Database(DB_NAME).Collection(COLL_NAME)
-    // Retrieves the first matching document
-    var r MongoSequence
-    filter := bson.D{{"name", SEQUENCE_NAME}}
-    err := coll.FindOne(context.TODO(), filter).Decode(&r)
-    if errors.Is(err, mongo.ErrNoDocuments) {
-        newSequence := MongoSequence{Name: SEQUENCE_NAME, Value: 0}
-        _, err = coll.InsertOne(context.TODO(), newSequence)
-        return 0, err
-    }
-    if err != nil {
-        return 0, err
-    }
-    return r.Value, err
+	coll := s.client.Database(DB_NAME).Collection(COLL_NAME)
+	// Retrieves the first matching document
+	var r MongoSequence
+	filter := bson.D{{"name", SEQUENCE_NAME}}
+	err := coll.FindOne(context.TODO(), filter).Decode(&r)
+	if errors.Is(err, mongo.ErrNoDocuments) {
+		newSequence := MongoSequence{Name: SEQUENCE_NAME, Value: 0}
+		_, err = coll.InsertOne(context.TODO(), newSequence)
+		return 0, err
+	}
+	if err != nil {
+		return 0, err
+	}
+	return r.Value, err
 }
 
 func (s *MongoSequencer) Save(seq int64) error {
-    filter := bson.D{{"name", SEQUENCE_NAME}}
-    update := bson.D{{"$set", bson.D{{"value", seq}}}}
-    coll := s.client.Database(DB_NAME).Collection(COLL_NAME)
-    _, err := coll.UpdateOne(context.TODO(), filter, update)
-    return err
+	filter := bson.D{{"name", SEQUENCE_NAME}}
+	update := bson.D{{"$set", bson.D{{"value", seq}}}}
+	coll := s.client.Database(DB_NAME).Collection(COLL_NAME)
+	_, err := coll.UpdateOne(context.TODO(), filter, update)
+	return err
 }
 
 func (s *MongoSequencer) Reset() error {
-    coll := s.client.Database(DB_NAME).Collection(COLL_NAME)
-    filter := bson.D{{"name", SEQUENCE_NAME}}
-    _, err := coll.DeleteOne(context.TODO(), filter)
-    return err
+	coll := s.client.Database(DB_NAME).Collection(COLL_NAME)
+	filter := bson.D{{"name", SEQUENCE_NAME}}
+	_, err := coll.DeleteOne(context.TODO(), filter)
+	return err
 }
 
 type MemorySequencer struct {
-    seq int64
+	seq int64
 }
 
 func NewMemorySequencer() (*MemorySequencer, error) {
-    return &MemorySequencer{}, nil
+	return &MemorySequencer{}, nil
 }
 
 func (s *MemorySequencer) Current() (int64, error) {
-    return s.seq, nil
+	return s.seq, nil
 }
 
 func (s *MemorySequencer) Save(seq int64) error {
-    s.seq = seq
-    return nil
+	s.seq = seq
+	return nil
 }
 
 func (s *MemorySequencer) Reset() error {
-    s.seq = 0
-    return nil
+	s.seq = 0
+	return nil
 }
